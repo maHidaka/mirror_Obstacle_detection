38348877a49bc9700a4d4815a13d89bdc46621b7
maHidaka <m.hdk816@gmail.com>
2021-07-27 19:44:51 +0900

    add/get target data from scan

--- mirror_obstacle_detection_apps/src/pose_estimate.cpp
+++ mirror_obstacle_detection_apps/src/pose_estimate.cpp
@@ -16,6 +16,7 @@ public:
     RosWithClass();
     void Publication(void);
     void Callback(const sensor_msgs::LaserScan::ConstPtr &msg);
+    void filter(sensor_msgs::LaserScan input);
 };
 
 RosWithClass::RosWithClass()
@@ -23,6 +24,44 @@ RosWithClass::RosWithClass()
     //  pub_m = nh.advertise<std_msgs::String>("/pub_msg", 1);
     pub = nh.advertise<sensor_msgs::LaserScan>("/scan_set", 1);
     sub = nh.subscribe("scan", 5, &RosWithClass::Callback, this);
+}
+
+void RosWithClass::filter(sensor_msgs::LaserScan input)
+{
+
+    int start_pos, target_size;
+    float start_angle = -2.35;
+    float end_angle = 0.0;
+    /*
+    start_pos = (input.angle_min - start_angle) / input.angle_increment;
+    target_size = (end_angle - start_angle) / input.angle_increment;
+*/
+    scan_msg = input;
+    //start_pos = (input.angle_min - start_angle) / input.angle_increment;
+
+    ROS_INFO("ranges.size() %d", scan_msg.ranges.size());
+    ROS_INFO("angle_min %f", scan_msg.angle_min);
+    ROS_INFO("angle_max %f", scan_msg.angle_max);
+    start_pos = int(start_angle - input.angle_min) / input.angle_increment;
+    target_size = (end_angle - start_angle) / input.angle_increment;
+    ROS_INFO("start_pos %d", start_pos);
+    ROS_INFO("target_size %d", target_size);
+    scan_msg.ranges.erase(scan_msg.ranges.begin(), scan_msg.ranges.begin() + int(start_pos));
+    scan_msg.ranges.erase(scan_msg.ranges.begin() + target_size, scan_msg.ranges.end());
+
+    scan_msg.angle_min = start_angle;
+    scan_msg.angle_max = end_angle;
+    //scan_msg.ranges.erase(scan_msg.ranges.begin() + target_size, scan_msg.ranges.end());
+    /*
+    scan_msg.ranges.erase(scan_msg.ranges.begin(), scan_msg.ranges.begin() + start_pos);
+    scan_msg.ranges.erase(scan_msg.ranges.begin() + target_size, scan_msg.ranges.end());
+    scan_msg.range_min = start_angle;
+    scan_msg.range_max = end_angle;
+    */
+    ROS_INFO("_ranges.size() %d", scan_msg.ranges.size());
+    ROS_INFO("angle_min %f", scan_msg.angle_min);
+    ROS_INFO("angle_max %f", scan_msg.angle_max);
+    ROS_INFO("-----------------------------------------");
 }
 
 void RosWithClass::Publication(void)
@@ -46,14 +85,15 @@ void RosWithClass::Callback(const sensor_msgs::LaserScan::ConstPtr &msg)
     {
         //ROS_INFO("front-range: %0.3f",msg->ranges[msg->ranges.size() / 2]);
     }
-
+    /*
     ROS_INFO("range.size %d", msg->ranges.size());
     ROS_INFO("angle_min %f", msg->angle_min * 57.3);
     ROS_INFO("angle_max %f", msg->angle_max * 57.3);
     ROS_INFO("angle_increment %f", msg->angle_increment * 57.3);
+    */
     float num = (msg->angle_max - msg->angle_min) / msg->angle_increment;
-    ROS_INFO("angle_num %f", num);
-    scan_msg = *msg;
+    //ROS_INFO("angle_num %f", num);
+    filter(*msg);
 }
 
 int main(int argc, char **argv)
