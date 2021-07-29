c281f3bdcc9170cd6bc1470499b3e6046baa7eef
maHidaka <m.hdk816@gmail.com>
2021-07-30 01:41:50 +0900

    publish fit line L

--- mirror_obstacle_detection_apps/scripts/fitting.py
+++ mirror_obstacle_detection_apps/scripts/fitting.py
@@ -2,13 +2,11 @@
 # -*- coding: utf-8 -*-
 
 import rospy
-from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import Point
 from visualization_msgs.msg import Marker
-from rospy.names import canonicalize_name
 from sensor_msgs.msg import LaserScan
 from std_msgs.msg import String
 from std_msgs.msg import Header
-from nav_msgs.msg import Path
 
 import numpy as np
 from matplotlib import pyplot as plt
@@ -21,14 +19,12 @@ class Fitting():
         # Publisherの作成
         self.pub = rospy.Publisher('fit_L', Marker, queue_size=1)
 
-    def callback(self, data):
-        # callback関数の処理をかく
-        # print(data)
-        self.calc_fitting_curve(data)
+        self.count = 0
 
-        # print(marker)
-        #marker.header.stamp = data.header.stamp
-        # self.Publisher(marker)
+    def callback(self, data):
+        fit_func = self.calc_fitting_curve(data)
+        marker = self.calc_marker(fit_func, data.header.stamp)
+        self.Publisher(marker)
 
     def Publisher(self, data):
         self.pub.publish(data)
@@ -39,8 +35,6 @@ class Fitting():
         y = []
         for range in data.ranges:
             angle = angle + data.angle_increment
-            #print(range, angle)
-            #print(self.getXY(range, angle))
             position = self.getXY(range, angle)
 
             if not np.isnan(position[0]):
@@ -49,9 +43,11 @@ class Fitting():
             else:
                 print("range value is NaN")
 
-        #print(x, y)
         func = np.polyfit(np.array(x), np.array(y), 1)
         print(func)
+        pos = list(func)
+        pos.extend([x[0], y[0], x[-1], y[-1]])
+        return pos
 
     def getXY(self, r, rad):
         # 度をラジアンに変換
@@ -59,35 +55,51 @@ class Fitting():
         y = r * np.sin(rad)
         return x, y
 
-    def calc_marker(self, position, id, time):
+    def calc_marker(self, pos, time):
+        self.count += 1
         marker_data = Marker()
         marker_data.header.frame_id = "laser"
         marker_data.ns = "soiya"
-        marker_data.id = id
+        marker_data.id = 0
         marker_data.header.stamp = time
         marker_data.action = Marker.ADD
 
-        marker_data.pose.position.x = position[0]
-        marker_data.pose.position.y = position[1]
+        marker_data.pose.position.x = 0.0
+        marker_data.pose.position.y = 0.0
         marker_data.pose.position.z = 0.0
 
         marker_data.pose.orientation.x = 0.0
         marker_data.pose.orientation.y = 0.0
-        marker_data.pose.orientation.z = 1.0
-        marker_data.pose.orientation.w = 0.0
+        marker_data.pose.orientation.z = 0.0
+        marker_data.pose.orientation.w = 1.0
 
-        marker_data.color.r = 1.0
-        marker_data.color.g = 0.0
+        marker_data.color.r = 0.0
+        marker_data.color.g = 1.0
         marker_data.color.b = 0.0
         marker_data.color.a = 1.0
 
-        marker_data.scale.x = 0.1
-        marker_data.scale.y = 0.01
-        marker_data.scale.z = 0.01
+        marker_data.scale.x = 0.005
+        marker_data.scale.y = 0.005
+        marker_data.scale.z = 0.005
 
+        #marker_data.lifetime = 0
         marker_data.lifetime = rospy.Duration()
-        marker_data.type = 0
-        #marker_data.header.stamp = data.header.stamp
+        marker_data.type = Marker.LINE_STRIP
+
+        marker_data.points = []
+
+        first_point = Point()
+        first_point.x = pos[2]
+        first_point.y = pos[0]*pos[2]+pos[1]
+        first_point.z = 0.0
+        marker_data.points.append(first_point)
+
+        second_point = Point()
+        second_point.x = pos[4]
+        second_point.y = pos[0]*pos[4]+pos[1]
+        second_point.z = 0.0
+        marker_data.points.append(second_point)
+
         return marker_data
 
 
